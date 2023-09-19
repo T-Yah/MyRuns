488b951ecf3596dadd9954fed2e3a41605484166
T-Yah <teeyali242@gmail.com>
2023-09-18 18:10:11 -0700

    ugly toolbar, save of sharedpreference, might need to debug types, working on onLoad

--- app/src/main/java/com/example/myruns/MainActivity.kt
+++ app/src/main/java/com/example/myruns/MainActivity.kt
@@ -1,34 +1,80 @@
 package com.example.myruns
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+
+        //loadProfile() //load the information back onto the screen if there is
 
         //On Click Listeners for Buttons
-        val cancelBtn = findViewById<Button>(R.id.cancelBtn);
+        val cancelBtn = findViewById<Button>(R.id.cancelBtn)
         cancelBtn.setOnClickListener{
             cancelCheck()
         }
-        val saveBtn = findViewById<Button>(R.id.saveBtn);
+        val saveBtn = findViewById<Button>(R.id.saveBtn)
         saveBtn.setOnClickListener{
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            if (ifSavable()){
+                saveAction()
+                Toast.makeText(this@MainActivity, "To Be Saved.", Toast.LENGTH_SHORT).show()
+
+            }
         }
-        val changeBtn = findViewById<Button>(R.id.changePfpBtn);
+        val changeBtn = findViewById<Button>(R.id.changePfpBtn)
         changeBtn.setOnClickListener{
             Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun loadProfile(){
+        //Grab Objects on Page
+        val nameField = findViewById<EditText>(R.id.enterName)
+        val emailField = findViewById<EditText>(R.id.enterEmail)
+        val phoneField = findViewById<EditText>(R.id.enterPhone)
+        val classField = findViewById<EditText>(R.id.enterClass)
+        val majorField = findViewById<EditText>(R.id.enterMajor)
+        val radioF = findViewById<RadioButton>(R.id.femaleRadioBtn)
+        val radioM = findViewById<RadioButton>(R.id.maleRadioBtn)
+
+        val sharedPref = getSharedPreferences("sharedPref", MODE_PRIVATE)
+
+        val name = sharedPref.getString("name", "")
+        val email = sharedPref.getString("email", "")
+        val phone = sharedPref.getString("phone", "")
+        val className = sharedPref.getString("class", "")
+        val major = sharedPref.getString("major", "")
+        val genderF = sharedPref.getBoolean("female", false)
+        val genderM = sharedPref.getBoolean("male", false)
+
+        nameField.setText(name)
+        emailField.setText(email)
+        phoneField.setText(phone)
+        classField.setText(className)
+        majorField.setText(major)
+
+        if(genderF){
+            radioF.isChecked = true
+        }
+        if(genderM){
+            radioM.isChecked = true
+        }
+    }
     //ref: https://www.javatpoint.com/kotlin-android-alertdialog#:~:text=Builder%20class%20call%20the%20setTitle,neutral%20and%20negative%20action%20respectively.
+    //Function to check if user actually wants to do the cancel option
     private fun cancelCheck(){
         //Make sure user wants to clear with alert dialog
         val builder = AlertDialog.Builder(this)
@@ -47,6 +93,7 @@ class MainActivity : AppCompatActivity() {
         alertDialog.show()
     }
 
+    //Function to preform the cancel option, will clear all displayed input
     private fun cancelAction(){
         //Grab Objects on Page
         val nameField = findViewById<EditText>(R.id.enterName)
@@ -65,5 +112,50 @@ class MainActivity : AppCompatActivity() {
         phoneField.text.clear()
         classField.text.clear()
         majorField.text.clear()
+    }
+    private fun ifSavable() : Boolean {
+        //Grab Objects on Page
+        val nameField = findViewById<EditText>(R.id.enterName)
+        val emailField = findViewById<EditText>(R.id.enterEmail)
+        val phoneField = findViewById<EditText>(R.id.enterPhone)
+        val classField = findViewById<EditText>(R.id.enterClass)
+        val majorField = findViewById<EditText>(R.id.enterMajor)
+        val radioF = findViewById<RadioButton>(R.id.femaleRadioBtn)
+        val radioM = findViewById<RadioButton>(R.id.maleRadioBtn)
+
+        if(TextUtils.isEmpty(nameField.getText().toString())
+            || TextUtils.isEmpty(emailField.getText().toString())
+            ||TextUtils.isEmpty(phoneField.getText().toString())
+            ||TextUtils.isEmpty(classField.getText().toString())
+            ||TextUtils.isEmpty(majorField.getText().toString())
+            ||(radioF.isChecked == false && radioM.isChecked == false)) {
+            Toast.makeText(this@MainActivity, "All fields must be filled before saving.", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        else{
+            return true
+        }
+    }
+    private fun saveAction(){
+        //Grab Objects on Page
+        val nameField = findViewById<EditText>(R.id.enterName)
+        val emailField = findViewById<EditText>(R.id.enterEmail)
+        val phoneField = findViewById<EditText>(R.id.enterPhone)
+        val classField = findViewById<EditText>(R.id.enterClass)
+        val majorField = findViewById<EditText>(R.id.enterMajor)
+        val radioF = findViewById<RadioButton>(R.id.femaleRadioBtn)
+        val radioM = findViewById<RadioButton>(R.id.maleRadioBtn)
+
+        val sharedPreference = getSharedPreferences("sharedPref", MODE_PRIVATE)
+        var editor = sharedPreference.edit()
+        //.getText().toString() changes from edittext to string
+        editor.putString("name", nameField.getText().toString())
+        editor.putString("email", emailField.getText().toString())
+        editor.putInt("phone", Integer.parseInt(phoneField.getText().toString()))
+        editor.putInt("class", Integer.parseInt(classField.getText().toString()))
+        editor.putString("major", majorField.getText().toString())
+        editor.putBoolean("female", radioF.isChecked());
+        editor.putBoolean("male", radioM.isChecked());
+        editor.commit()
     }
 }
