133ebd268007c7d826a06ebad994a9676ae886ff
T-Yah <teeyali242@gmail.com>
2023-09-30 17:02:44 -0700

    ui completed for start fragment, working on launching activity from start page

--- app/src/main/java/com/example/teeya_li/MapActivity.kt
+++ app/src/main/java/com/example/teeya_li/MapActivity.kt
@@ -2,15 +2,17 @@ package com.example.teeya_li
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myruns.R
 
-class MapActivity : ComponentActivity() {
+class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
 
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         toolbar.setTitleTextColor(Color.WHITE);
     }
+
 }
--- app/src/main/java/com/example/teeya_li/fragments/StartFragment.kt
+++ app/src/main/java/com/example/teeya_li/fragments/StartFragment.kt
@@ -1,19 +1,79 @@
 package com.example.teeya_li.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import com.example.myruns.R
+import com.example.teeya_li.MapActivity
 
 class StartFragment : Fragment() {
+    private lateinit var InputSpinner: Spinner
+    private lateinit var ActivitySpinner: Spinner
+    private var input_type_options = arrayOf("Manual Entry", "GPS", "Automatic")
+    private var activity_type_options = arrayOf("Running", "Walking", "Standing", "Cycling", "Hiking", "Downhill Skiing",
+        "Cross-Country Skiing", "Snowboarding", "Skating", "Swimming", "Mountain Biking", "Wheelchair", "Elliptical",
+        "Other")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        val view = inflater.inflate(R.layout.fragment_start, container, false)
+
+        // Initialize the spinner & set it up
+        InputSpinner = view.findViewById(R.id.input_type_spinner)
+        setInputSpinner()
+
+        ActivitySpinner = view.findViewById(R.id.activity_type_spinner)
+        setActivitySpinner()
+
+        val startBtn = view.findViewById<Button>(R.id.startBtn)
+        startBtn.setOnClickListener{
+            InputSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position == 1 || position == 2){
+                        val intent: Intent = Intent(requireContext(), MapActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // This method is called when nothing is selected in the Spinner.
+                    // You can leave it empty if you don't need to perform any action.
+                }
+            }
+        }
+
+        return view
+    }
+
+    private fun setActivitySpinner() {
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, activity_type_options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Set the created adapter to the Spinner
+        ActivitySpinner.adapter = adapter
+    }
+
+    private fun setInputSpinner() {
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, input_type_options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Set the created adapter to the Spinner
+        InputSpinner.adapter = adapter
     }
 
 }
